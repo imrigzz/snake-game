43df5e63ebdabee293ea68f86a050fa02afae927
imrigzz <rizinchuk@gmail.com>
2022-03-21 19:03:58 +0530

    init

---
+++ app.js
@@ -0,0 +1,129 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext('2d');
+
+
+class SnakePart{
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+  }
+}
+
+let speed = 7;
+let tileCount = 20;
+let tileSize = canvas.width/tileCount-2;
+let headX = 10;
+let headY = 10;
+let xVelocity = 0;
+let yVelocity = 0;
+let tailLength = 2;
+const snakeParts =[];
+
+let appleX = 5;
+let appleY = 5;
+let score = 0;
+
+
+
+///game loop
+function drawGame(){
+  changeSnakePosition();
+  let result = isGameOver();
+  if(result){
+    return;
+  }
+
+  clearScreen();
+
+  checkAppleCollision();
+  drawApple();
+  drawSnake(); 
+  drawScore();
+  setTimeout(drawGame, 1000/speed);
+}
+
+function drawScore(){
+  ctx.fillStyle = 'white';
+  ctx.font="10px";
+  ctx.fillText("Score" + score, canvas.width-50, 10);
+
+}
+function clearScreen(){
+  ctx.fillStyle= 'black';
+  ctx.fillRect(0,0,canvas.width, canvas.height);
+}
+
+function drawSnake(){
+
+  ctx.fillStyle = 'green';
+  for(i=0; i<snakeParts.length; i++){
+    let part = snakeParts[i];
+    ctx.fillRect(part.x * tileCount, part.y*tileCount, tileSize, tileSize);
+  }
+
+  snakeParts.push(new SnakePart(headX, headY)); 
+  if (snakeParts.length>tailLength){
+    snakeParts.shift(); //remve the futherest item from the snake parts
+  }
+
+  ctx.fillStyle = 'orange'
+  ctx.fillRect(headX * tileCount, headY*tileCount, tileSize, tileSize)
+
+}
+
+function changeSnakePosition(){
+  headX=headX +xVelocity;
+  headY = headY + yVelocity;
+}
+
+function drawApple(){
+  ctx.fillStyle= 'red'
+  ctx.fillRect (appleX* tileCount, appleY * tileCount, tileSize, tileSize)
+}
+
+function checkAppleCollision(){
+  if(appleX == headX && appleY == headY){
+    appleX = Math.floor(Math.random()* tileCount);
+    appleY = Math.floor(Math.random() * tileCount);
+    tailLength++;
+    score++;
+  }
+}
+
+document.body.addEventListener('keydown', keyDown);
+
+function keyDown (event){
+  //up
+  if(event.keyCode == 38){
+    if(yVelocity ==1)
+    return;
+    yVelocity = -1;
+    xVelocity = 0;
+  }
+  
+  //down
+  if(event.keyCode ==40){
+    if(yVelocity == -1)
+    return;
+    yVelocity = 1;
+    xVelocity = 0;
+  }
+
+   //left
+   if(event.keyCode ==37){
+    if(xVelocity == 1)
+    return;
+    yVelocity = 0;
+    xVelocity = -1;
+  }
+
+  //right
+  if(event.keyCode ==39){
+    if(xVelocity ==-1)
+    return;
+    yVelocity = 0;
+    xVelocity = 1;
+  }
+}
+
+drawGame();
